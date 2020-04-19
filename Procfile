a2e58c6eaaa2c52cefda1347db7eafac059f370a
Akash Agrawal <akashrocksha@gmail.com>
2020-04-19 14:37:49 +0530

    Added deployment files

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn app.wsgi:application --log-file - --log-level debug
+release: python manage.py migrate
--- app/settings.py
+++ app/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -25,7 +27,7 @@ SECRET_KEY = 'i)oa#b$%eg#%$c)s*e7n2h4e^gkecnvrz2+a7fufr+y-wf_)ys'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -133,3 +135,6 @@ REST_FRAMEWORK = {
     ],
     'UNAUTHENTICATED_USER': None
 }
+
+
+django_heroku.settings(locals())
